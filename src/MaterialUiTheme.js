f3f4badb2030ac236f2e49a7bc54eefee73f412f
Artem Khasanov <artemkhasanov@MacMiniAdmin-Khasanov.local>
2017-09-13 09:34:52 +0300

    mUI update

--- src/MaterialUiTheme.js
+++ src/MaterialUiTheme.js
@@ -1,16 +1,16 @@
-import { createMuiTheme  } from 'material-ui/styles'
-import createPalette from 'material-ui/styles/palette'
-import purple from 'material-ui/colors/purple'
-import green from 'material-ui/colors/green'
-import red from 'material-ui/colors/red'
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
+import red from 'material-ui/colors/red';
 
-export default createMuiTheme({
-  palette: createPalette({
-    primary: purple,
-    accent: {
+const theme = createMuiTheme({
+  palette: {
+    primary: purple, 
+    secondary: {
       ...green,
       A400: '#00e677',
     },
     error: red,
-  }),
-})
+  },
+});
